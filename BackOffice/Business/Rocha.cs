56af1f4850c456d532b9895d8403572c5497aac4
PPinto22 <pedropinto24@hotmail.com>
2016-06-09 18:43:59 +0100

    ...

---
+++ BackOffice/Business/Rocha.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Business
+{
+    public class rocha
+    {
+        private string designacao { get; set; }
+        private string tipo { get; set; }
+        private float peso { get; set; }
+        private string textura { get; set; }
+        private string cor { get; set; }
+
+        public rocha() { }
+        public rocha(string designacao, string tipo, float peso, string textura, string cor)
+        {
+            this.designacao = designacao;
+            this.tipo = tipo;
+            this.peso = peso;
+            this.textura = textura;
+            this.cor = cor;
+        }
+    }
+}
---
+++ BackOffice/Business/mineral.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Business
+{
+    class mineral
+    {
+        private string designacao { get; set; }
+        private float peso { get; set; }
+        private string risca { get; set; }
+        private string cor { get; set; }
+
+        public mineral() { }
+        public mineral(string designacao, float peso, string risca, string cor)
+        {
+            this.designacao = designacao;
+            this.peso = peso;
+            this.risca = risca;
+            this.cor = cor;
+        }
+    }
+}
